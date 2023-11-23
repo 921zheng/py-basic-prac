fb27b21738f5cb69ed67c7015b909d28e165f14b
Sophie Zheng <139349595+921zheng@users.noreply.github.com>
2023-11-23 14:00:08 +0000

    Add files via upload

---
+++ py-basic_prac.py
@@ -0,0 +1,104 @@
+# #check type
+# print(type("Hi"))
+# print(type(123))
+# print(type(123+2j))
+# print(type(2j))
+
+#int,float,complex
+
+# a=10
+# b=45.3
+# c=3+2j
+# x=float(a)
+# y=int(b)
+# z=complex(c)
+# print(x)
+# print(y)
+# print(z)
+
+# I=complex(10.1)
+# print(I)
+# I=complex(10)
+# print(I)
+# I=complex(10+2+2j)
+# print(I)
+
+#random
+
+# import random
+# # print(random.randrange(1,4))
+
+# list=["A","B","C"]
+# print(random.choice(list))
+
+#index
+# fruits=["A","B","C","D"]
+# print(fruits.index("C"))
+
+#legnth
+# a="hi james"
+# print(len(a))
+
+# a="Hello World!"
+# print(a[2:5])
+# print(a[-5:-2])
+# print(a[:2])
+# print(a[2:])
+
+#upper and lower case
+# a="fhks dsf"
+# print(a.upper().lower())
+
+#replace
+# a="fdhishfi ihfid ihfds hih"
+# print(a.replace("i","z"))
+
+#strip
+# a="  fsklf  fkdhs  "
+# print(a.strip())
+
+#split
+# a="hello, worfdlsj fdlsjfsdlj "
+# print(a.split(" "))
+# print(a.split(","))
+# print(a.split("o"))
+
+#format
+#1
+# score=90
+# msg="James got {}"
+# print(msg.format(score))
+#2
+# print("james has {fruit}".format(fruit="apple"))
+#3
+# age=40
+# name="sfds"
+# number=3
+# msg="{} is {} and he did {}"
+# print(msg.format(name,age,number))
+
+age=40
+name="sfds"
+number=3
+msg="{1} is {0} and he did {2}"
+print(msg.format(age,name,number))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
